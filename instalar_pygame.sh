9507c1186dc3594df4dc0de57456f4da741c985a
Gabriel Costa de Moraes <121687389+GabrielCM16@users.noreply.github.com>
2025-07-21 13:06:47 -0300

    Create instalar_pygame.sh

---
+++ instalar_pygame.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt update && sudo apt install -y python3-pygame python3-dev python3-pip python3-setuptools libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libportmidi-dev libfreetype6-dev libswscale-dev libavformat-dev libavcodec-dev libjpeg-dev libpng-dev libtiff-dev libx11-dev
